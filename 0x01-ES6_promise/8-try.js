25e8f164818d87f2b444ec124aac8ac7d99fe576
marylizkarumba <marylizkarumba791@gmail.com>
2023-01-28 00:03:48 +0300

    trial

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -2,6 +2,6 @@
 error with the message cannot divide by 0. Otherwise it should return the
 numerator divided by the denominator */
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) throw new Error('cannot divide by 0');
-    return numerator / denominator;
-  }
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return numerator / denominator;
+}
